195df3c9941b2163ab09f22671a1926518cda97d
Gilvan Alencar <48884274+Gil135@users.noreply.github.com>
2022-07-24 20:11:02 -0300

    primeira commit

---
+++ criar_user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+sudo apt update
+
+sudo apt install git
+
+
+
+echo "Criando usuarios"
+useradd novo_usuario -c "Novo Usuario" -s /bin/bash -m -p $(openssl passwd -crypt  Senha123)
+passwd novo_usuario -e
+
+useradd usuario2 -c "Usuario 2" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd usuario2 -e
